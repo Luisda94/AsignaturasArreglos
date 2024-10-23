72081d891a0b49c3b19808a04b85b1c0485e8246
Luisda <luisda915@gmail.com>
2024-10-23 20:22:19 -0300

    add para limpiar el buffer y asi ingresar varias materias, se modifica para ingresar los nombres de la materia.

--- src/Asignaturas/Asignaturas.java
+++ src/Asignaturas/Asignaturas.java
@@ -7,22 +7,29 @@ public class Asignaturas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner Mi_escaner = new Scanner(System.in); 
-		String[] asignaturas = {"Matemáticas", "Historia", "Ciencias", "Lenguaje", "Arte"};
-		 Double notas[];
+		String[] asignaturas = new String[5];  // Arreglo 1: nombres de las asignaturas
+		Double notas[];
 		//agrego solo el tamaño de notas de forma vacio
 		 notas = new Double[5];
 		  
 		// Ingresar las notas para cada asignatura
-	        for (int i = 0; i < notas.length; i++) {
+	        // Ingresar los nombres de las asignaturas y sus respectivas notas
+	        for (int i = 0; i < asignaturas.length; i++) {
+	            System.out.printf("Ingrese el nombre de la materia %d: ", i + 1);
+	            asignaturas[i] = Mi_escaner.nextLine();  // Captura el nombre de la asignatura
+	            
 	            System.out.printf("Ingrese la nota para %s: ", asignaturas[i]);
 	            notas[i] = Mi_escaner.nextDouble();
+	            Mi_escaner.nextLine();//esto limplia el buffer.
 	        }
 
-	        // Calcular el promedio final
+	        // Inicializa en cero para poder sacar el promedio
 	        double promedioFinal = 0.0;
+	        //Cuenta los valores ingresados para luego de sumarlos
 	        for (int i = 0; i < notas.length; i++) {
 	            promedioFinal += notas[i];
 	        }
+	        //Calcular el promedio final
 	        promedioFinal /= notas.length;
 	
 	        // Mostrar el promedio obtenido
