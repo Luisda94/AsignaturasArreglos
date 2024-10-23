5ec1cb5dfcd737628ad52a627d45acd8230af6ce
Luisda <luisda915@gmail.com>
2024-10-23 19:12:36 -0300

    se crea el arreglo Asignatura y el el arreglo notas

--- src/Asignaturas/Asignaturas.java
+++ src/Asignaturas/Asignaturas.java
@@ -6,8 +6,17 @@ public class Asignaturas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner Mi_escaner = new Scanner(System.in);
-		System.out.println("Ingrese la asigntarua: ");
+		Scanner Mi_escaner = new Scanner(System.in); 
+		String[] asignaturas = {"Matemáticas", "Historia", "Ciencias", "Lenguaje", "Arte"};
+		 Double notas[];
+		  notas = new Double[5];
+		 notas [0] = Mi_escaner.nextDouble();
+		 notas [1] = Mi_escaner.nextDouble();
+		 notas [2] = Mi_escaner.nextDouble();
+		 notas [3] = Mi_escaner.nextDouble();
+		 notas [4] = Mi_escaner.nextDouble();
+		 
+		 System.out.println("Ingrese el nombre de la  asigntarua: ");
 		
 		String Asignatura = Mi_escaner.nextLine();
 
