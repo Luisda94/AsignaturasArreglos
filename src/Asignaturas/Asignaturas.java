c2add0d644f3bf1181d6df406b350f98c5df2672
Luisda <luisda915@gmail.com>
2024-10-23 16:48:49 -0300

    add Asignatura

--- src/Asignaturas/Asignaturas.java
+++ src/Asignaturas/Asignaturas.java
@@ -1,10 +1,18 @@
 package Asignaturas;
 
+import java.util.Scanner;
+
 public class Asignaturas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner Mi_escaner = new Scanner(System.in);
+		System.out.println("Ingrese la asigntarua: ");
+		
+		String Asignatura = Mi_escaner.nextLine();
 
+
+Mi_escaner.close();
 	}
 
 }
